134b21a62f64beefd2453af11b83e2f5a7e6cb9e
25081997pc <chavanpooja2508@gmail.com>
2022-07-27 13:55:40 +0530

    Using Add and Display Method

--- DataStructureDemo/Program.cs
+++ DataStructureDemo/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to The Data Structure Program");
+            Console.WriteLine("Welcome to The LinkedList Program");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+            Console.ReadKey();
         }
     }
 }
