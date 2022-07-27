697c636200090a826fdc9050eb89eac040b436bc
25081997pc <chavanpooja2508@gmail.com>
2022-07-27 16:49:56 +0530

    Create Queue

---
+++ QueueUsingLinkedList/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace QueueUsingLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom to the Queue Program where we using queue Operations");
+            LinkedListQueue linkedListQueue = new LinkedListQueue();
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
+
+            linkedListQueue.Display();
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
