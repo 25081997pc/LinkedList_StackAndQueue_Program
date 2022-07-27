8d68cfc6010ccab8830208ceb23accf62059760f
25081997pc <chavanpooja2508@gmail.com>
2022-07-27 16:24:14 +0530

    Creating Stack

---
+++ StackUsingLinkedList/LinkedListStack.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackUsingLinkedList
+{
+    internal class LinkedListStack
+    {
+        private Node top;
+        
+        public LinkedListStack()
+        {
+            this.top = null;
+        }
+        //This method is created to Pushing the Elements into Stack
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+                node.next = null;
+            else
+                node.next = this.top;
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack ", value);
+        }
+        //This method is created to Display the Stack Elemnets
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " "); //56 30 70 
+                temp = temp.next;
+            }
+        }   
+    }
+}
---
+++ StackUsingLinkedList/Program.cs
@@ -0,0 +1,19 @@
+﻿
+namespace StackUsingLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to The Stack Program where we using Stack Operations");
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+
+            linkedListStack.Display();
+            Console.ReadKey();
+        }
+    }
+}
+
