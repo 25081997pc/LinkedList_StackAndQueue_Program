90e96bd8d8b3979c58fb2ca1f575efa7bf394b41
25081997pc <chavanpooja2508@gmail.com>
2022-07-27 14:11:13 +0530

    Creating LinkedList

--- DataStructureDemo/Program.cs
+++ DataStructureDemo/Program.cs
@@ -6,9 +6,9 @@
         {
             Console.WriteLine("Welcome to The LinkedList Program");
             LinkedList list = new LinkedList();
-            list.Add(56);
-            list.Add(30);
             list.Add(70);
+            list.Add(30);
+            list.Add(56);
             list.Display();
             Console.ReadKey();
         }
